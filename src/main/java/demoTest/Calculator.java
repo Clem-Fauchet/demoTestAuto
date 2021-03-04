f93e0363b1370a5e2b614b39349c7d3550747883
Clem-Fauchet <cfauchet.pro@gmail.com>
2021-03-04 12:35:32 +0100

    Initial commit

---
+++ src/main/java/demoTest/Calculator.java
@@ -0,0 +1,12 @@
+package demoTest;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return 0;
+	}
+
+	public int sub(int a, int b) {
+		return 0;
+	}
+}
---
+++ src/test/java/demoTest/CalculatorTest.java
@@ -0,0 +1,29 @@
+package demoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest extends Calculator {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
